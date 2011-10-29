e6c821921f2085e0317ebc56cd785bbd01c006b7
Martin Richter <me@martinrichter.net>
2011-10-29 20:22:59 +0200

    Initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jmd::Application.config.secret_token = '4447ae2f818f7a03efd8fa6ced6f56efbd0e132f06fa143cdf9a642d5a439f662760685e70872fc9de065e62985efcb562427eb061a9d91c552e4c22d7508c87'

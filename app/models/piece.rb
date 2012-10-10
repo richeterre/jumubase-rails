ad518473aecb4014708abb25fd942d85a87a6da0
Martin Richter <me@martinrichter.net>
2012-10-10 21:09:49 +0200

    Updated syntax to not use hash rockets

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -18,20 +18,20 @@ class Piece < ActiveRecord::Base
   attr_accessible :title, :composer_id, :performance_id, :epoch_id, :minutes, :seconds,
       :composer_attributes
   
-  belongs_to :composer, :dependent => :destroy # since every piece has its own composer
-  belongs_to :performance, :touch => true
+  belongs_to :composer, dependent: :destroy # since every piece has its own composer
+  belongs_to :performance, touch: true
   belongs_to :epoch
   
   accepts_nested_attributes_for :composer
   
-  validates :title,       :presence => true
-  validates :epoch_id,    :presence => true
-  validates :minutes,     :presence => true,
-                          :numericality => true,
-                          :inclusion => { :in => 0..45 }
-  validates :seconds,     :presence => true,
-                          :numericality => true,
-                          :inclusion => { :in => 0..59 }
+  validates :title,       presence: true
+  validates :epoch_id,    presence: true
+  validates :minutes,     presence: true,
+                          numericality: { only_integer: true },
+                          inclusion: { in: 0..45 }
+  validates :seconds,     presence: true,
+                          numericality: { only_integer: true },
+                          inclusion: { in: 0..59 }
                           
   def duration
     min = (minutes || 0)

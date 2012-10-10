68b01cf112c6d5c24f8a58c977b258d428ea88b0
Martin Richter <me@martinrichter.net>
2012-10-11 01:32:04 +0200

    Added request specs for signup form fields except for gender radio buttons

--- spec/requests/signup_spec.rb
+++ spec/requests/signup_spec.rb
@@ -20,6 +20,30 @@ describe "Signup page" do
 
     it { should have_select "Wettbewerb", options: ["Bitte wählen"] + @current_competitions.map(&:name) }
     it { should have_select "Kategorie", options: ["Bitte wählen"] + @active_categories.map(&:name) }
+    it { should have_field "Vorname", text: "" }
+    it { should have_field "Nachname", text: "" }
+    it { should have_select "Rolle", options: ["Bitte wählen"] + Role.all.map(&:name) }
+    it { should have_select "Instrument", options: ["Bitte wählen"] + Instrument.all.map(&:name) }
+    it { should have_select "Geburtsdatum" }
+    # it { should have_field "Geschlecht" }
+    it { should have_field "Straße", text: "" }
+    it { should have_field "Postleitzahl", text: "" }
+    it { should have_field "Ort", text: "" }
+    it { should have_select "Land", options: ["Bitte wählen"] + Country.all.map(&:name) }
+    it { should have_field "Telefon", text: "" }
+    it { should have_field "E-Mail", type: :email, text: "" }
+    it { should have_button "Teilnehmer entfernen" }
+    it { should have_button "Weiteren Teilnehmer hinzufügen" }
+    it { should have_field "Titel", text: "" }
+    it { should have_field "Komponist", text: "" }
+    it { should have_field "Geburtsjahr", text: "" }
+    it { should have_field "(Todesjahr)", text: "" }
+    it { should have_select "Epoche", options: ["Bitte wählen"] + Epoch.all.map(&:name) }
+    it { should have_field "Dauer ca.", type: :number, text: "" }
+    it { should have_field "", type: :number, text: "" }
+    it { should have_button "Stück entfernen" }
+    it { should have_button "Weiteres Stück hinzufügen" }
+    it { should have_button "Anmeldung absenden" }
 
     it "should allow only valid inputs for the minutes and seconds fields"
   end

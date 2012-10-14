63ddbe679bf2b88e5c8ec8cd4fbf1787a7e2ad59
Martin Richter <me@martinrichter.net>
2012-10-14 18:04:25 +0200

    Fixed alert matchers

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,11 +1,5 @@
 include ApplicationHelper
 
-RSpec::Matchers.define :have_success_message do |message|
-  match do |page|
-    page.should have_selector('div.alert.alert-info')
-  end
-end
-
 RSpec::Matchers.define :have_alert_message do |message|
   match do |page|
     page.should have_selector('div.alert')
@@ -15,5 +9,17 @@ end
 RSpec::Matchers.define :have_error_message do |message|
   match do |page|
     page.should have_selector('div.alert.alert-error')
+  end
+end
+
+RSpec::Matchers.define :have_success_message do |message|
+  match do |page|
+    page.should have_selector('div.alert.alert-success')
+  end
+end
+
+RSpec::Matchers.define :have_info_message do |message|
+  match do |page|
+    page.should have_selector('div.alert.alert-info')
   end
 end

2f38f97f7e5f36867e8234d9ef6ce532c701e33e
Martin Richter <me@martinrichter.net>
2012-11-10 00:22:16 +0100

    Updated test for shorter minimum password length

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -80,7 +80,7 @@ describe User do
   end
 
   describe "when password is too short" do
-    before { user.password = user.password_confirmation = "a" * 5 }
+    before { user.password = user.password_confirmation = "a" * 4 }
     it { should_not be_valid }
   end
 

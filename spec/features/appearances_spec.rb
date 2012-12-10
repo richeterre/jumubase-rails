92a80d60b3f4244fe21cf03f4d092a7f71c0ae71
Martin Richter <me@martinrichter.net>
2012-12-10 22:55:33 +0100

    Added tests for adding points

--- spec/features/appearances_spec.rb
+++ spec/features/appearances_spec.rb
@@ -10,7 +10,9 @@ describe "Appearances" do
     before do
       @host = FactoryGirl.create(:host)
       competition = FactoryGirl.create(:current_competition, host: @host)
-      @current_performances = FactoryGirl.create_list(:performance, 3, competition: competition)
+      FactoryGirl.create_list(:performance, 3, competition: competition)
+      FactoryGirl.create_list(:current_solo_acc_performance, 3, competition: competition)
+      FactoryGirl.create_list(:current_ensemble_performance, 3, competition: competition)
     end
 
     let(:user) { FactoryGirl.create(:user, hosts: [@host]) }
@@ -20,23 +22,48 @@ describe "Appearances" do
       context "when signed in as a regular user" do
         before do
           visit root_path
+          click_on "Interne Seiten"
           sign_in(user)
           visit jmd_appearances_path
         end
 
         it "should list all appearances from own hosts' competitions" do
-          @current_performances.each do |performance|
+          Performance.all.each do |performance|
             page.should have_selector "tbody tr > td[rowspan='#{performance.appearances.length}']",
                                       text: performance.category.name
 
             performance.appearances.each do |appearance|
-              page.should have_selector "tbody tr > td", text: appearance.participant.full_name
+              page.should have_selector "tbody tr",
+                                        text: "#{appearance.participant.full_name}, #{appearance.instrument.name}"
+              page.should have_selector "tbody tr > td > span.badge-info",
+                                        text: appearance.age_group
             end
           end
         end
+
+        it "should allow editing an appearance's points", js: true do
+          appearance = Performance.first.appearances.first
+
+          modal_id = "#appearance#{appearance.id}Modal"
+          find(:xpath, "//a[@href='#{modal_id}']").click
+
+          within(modal_id) do
+            fill_in "Punktzahl", with: 23
+            click_button "Punkte speichern"
+          end
+
+          page.should have_selector "tbody tr > td > span.badge.badge-important",
+                                    text: appearance.points
+          page.should have_selector "tbody tr > td",
+                                    text: appearance.prize
+          page.should have_selector "tbody tr > td > span.label.label-success",
+                                    text: "WL"
+        end
       end
 
-      context "when signed in as an admin"
+      context "when signed in as an admin" do
+        it "should list all appearances from current competitions"
+      end
     end
   end
 end

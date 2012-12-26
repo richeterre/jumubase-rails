f101d6634224549d4f07544dd3638b0ad3617ad3
Martin Richter <martin.richter@futurice.com>
2012-12-26 02:57:43 +0100

    Implemented CanCan authorization for appearances via their performance

--- app/controllers/jmd/appearances_controller.rb
+++ app/controllers/jmd/appearances_controller.rb
@@ -1,22 +1,23 @@
 # -*- encoding : utf-8 -*-
 class Jmd::AppearancesController < Jmd::BaseController
 
-  skip_authorization_check
+  load_and_authorize_resource :performance, parent: false, only: :index
 
   def index
-    @performances = Performance.current.visible_to(current_user)
-                               .category_order.paginate(page: params[:page], per_page: 15)
+    @performances = Performance.visible_to(current_user).category_order
+                               .paginate(page: params[:page], per_page: 15)
   end
 
   def update
-    appearance = Appearance.find(params[:id])
-    if Performance.visible_to(current_user).include? appearance.performance
-      appearance.accessible = :all # Allow editing if user can see appearance
-    end
-    if appearance.update_attributes(params[:appearance])
-      flash[:success] = "Die Punktzahl für #{appearance.participant.full_name} wurde gespeichert."
+    # @appearance is fetched by CanCan
+    @appearance = Appearance.find(params[:id])
+    authorize! :update, @appearance.performance
+
+    @appearance.points = params[:appearance][:points]
+    if @appearance.save
+      flash[:success] = "Die Punktzahl für #{@appearance.participant.full_name} wurde gespeichert."
     else
-      flash[:error] = "Die Punktzahl für #{appearance.participant.full_name} konnte nicht gespeichert werden."
+      flash[:error] = "Die Punktzahl für #{@appearance.participant.full_name} konnte nicht gespeichert werden."
     end
     redirect_to :back
   end

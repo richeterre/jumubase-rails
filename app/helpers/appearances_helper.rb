95c161442e8550e9c07f35afdb55a7362fe14fc0
Martin Richter <martin.richter@futurice.com>
2013-03-02 12:08:06 +0100

    Fix copy-pasted comment

--- app/helpers/appearances_helper.rb
+++ app/helpers/appearances_helper.rb
@@ -1,5 +1,5 @@
 module AppearancesHelper
-  # Return whether a performance filter is currently active
+  # Return a badge with the appearance's age group
   def age_group_badge_tag(appearance)
     if appearance.accompaniment?
       info_badge_tag(appearance.age_group)

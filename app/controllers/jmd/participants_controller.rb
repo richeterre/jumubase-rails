c153dd5bc52f8a70d8342d7eba06fd3e0d987be0
Martin Richter <me@martinrichter.net>
2015-02-01 20:38:16 +0100

    Set custom filename with random number for CSV participant exports

--- app/controllers/jmd/participants_controller.rb
+++ app/controllers/jmd/participants_controller.rb
@@ -1,5 +1,6 @@
 # -*- encoding : utf-8 -*-
 class Jmd::ParticipantsController < Jmd::BaseController
+  include ApplicationHelper
 
   load_and_authorize_resource :competition
   load_and_authorize_resource :participant, through: :competition
@@ -10,7 +11,7 @@ class Jmd::ParticipantsController < Jmd::BaseController
     @participants = @participants.includes(:country).order(:last_name)
 
     respond_to do |format|
-       format.csv { render csv: @participants }
+       format.csv { render csv: @participants, filename: "teilnehmer#{random_number}" }
        format.html
     end
   end

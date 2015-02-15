290593f96e9cb5c89e35e75a970e5059ef70725e
Martin Richter <me@martinrichter.net>
2015-02-15 14:34:59 +0100

    Re-add table for Venues to schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150215105933) do
+ActiveRecord::Schema.define(:version => 20150215132956) do
 
   create_table "appearances", :force => true do |t|
     t.integer  "performance_id"
@@ -183,5 +183,12 @@ ActiveRecord::Schema.define(:version => 20150215105933) do
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["remember_token"], :name => "index_users_on_remember_token"
+
+  create_table "venues", :force => true do |t|
+    t.string   "name"
+    t.integer  "host_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
 end

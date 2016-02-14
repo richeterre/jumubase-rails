659fde62d07a3cc94bc9c273d57ff8d328a1c8b7
Martin Richter <me@martinrichter.net>
2016-02-14 16:23:28 +0100

    Interpret date given to /performances endpoint as local date in competition's time zone

--- app/controllers/api/v1/performances_controller.rb
+++ app/controllers/api/v1/performances_controller.rb
@@ -3,7 +3,10 @@ module Api::V1
     def index
       competition = Competition.find(params[:competition_id])
       @venue = Venue.find(params[:venue_id])
-      @date = Date.strptime(params[:date], '%Y-%m-%d')
+
+      # Parse given date assuming competition's local time zone
+      local_tz = competition.host.time_zone
+      @date = local_tz.parse(params[:local_date])
 
       @performances = competition.staged_performances(@venue, @date)
         .includes(

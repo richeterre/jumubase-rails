f4eb891c6b06ad8a5f3ae03273e6dd9b2cc892d8
Martin Richter <me@martinrichter.net>
2016-10-23 23:35:33 +0200

    Annotate Participant model with latest changes

--- spec/models/participant_spec.rb
+++ spec/models/participant_spec.rb
@@ -2,18 +2,14 @@
 #
 # Table name: participants
 #
-#  id          :integer          not null, primary key
-#  first_name  :string(255)
-#  last_name   :string(255)
-#  birthdate   :date
-#  street      :string(255)
-#  postal_code :string(255)
-#  city        :string(255)
-#  country_id  :integer
-#  phone       :string(255)
-#  email       :string(255)
-#  created_at  :datetime         not null
-#  updated_at  :datetime         not null
+#  id         :integer          not null, primary key
+#  first_name :string(255)
+#  last_name  :string(255)
+#  birthdate  :date
+#  phone      :string(255)
+#  email      :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
 #
 
 require 'spec_helper'

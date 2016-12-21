e123c184a754c90a86675fa06e3dc96b319f1404
Martin Richter <martin.richter@futurice.com>
2016-12-21 12:53:05 +0100

    Optimize performance of a contest's participant list

--- app/controllers/jmd/participants_controller.rb
+++ app/controllers/jmd/participants_controller.rb
@@ -10,6 +10,19 @@ class Jmd::ParticipantsController < Jmd::BaseController
     # TODO: Why doesn't authorization work although contest can be accessed by user?
     @participants = @participants.order(:last_name)
 
+    performances = Performance
+      .in_contest(@contest)
+      .includes({ contest_category: :category }, :appearances)
+
+    # Create hash of participants with their associated performances for listing
+    @participant_performances = {}
+    performances.each do |performance|
+      performance.appearances.each do |appearance|
+        performances = @participant_performances[appearance.participant_id] ||= []
+        performances << performance
+      end
+    end
+
     respond_to do |format|
        format.csv { render csv: @participants, filename: "teilnehmer#{random_number}" }
        format.html

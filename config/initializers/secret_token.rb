b81c39224e7dc89627369f6c490964ee594010d1
Martin Richter <me@martinrichter.net>
2016-12-22 22:45:07 +0100

    Use env variable for secret token in production

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,5 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Jmd::Application.config.secret_token = '412a7ad3a5abd0f47f60ed481857dfc9c4386bf419416d9a28a21d99a24dd85a626ad95ad16f903d712cc84802267dc9d3abe2d908e89e9df515f702dff8e471'
+Jmd::Application.config.secret_token =
+  Rails.env.production? ? ENV['SECRET_TOKEN'] : 'x' * 30

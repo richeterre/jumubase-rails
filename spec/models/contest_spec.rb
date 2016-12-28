127ab9dff6f874ded84a8d838b2698fb55696a1e
Martin Richter <me@martinrichter.net>
2016-12-28 14:01:13 +0200

    Make tests run at all again

--- spec/models/contest_spec.rb
+++ spec/models/contest_spec.rb
@@ -1,9 +1,8 @@
 # == Schema Information
 #
-# Table name: competitions
+# Table name: contests
 #
 #  id                :integer          not null, primary key
-#  round_id          :integer
 #  host_id           :integer
 #  begins            :date
 #  ends              :date
@@ -11,17 +10,18 @@
 #  updated_at        :datetime         not null
 #  certificate_date  :date
 #  season            :integer
-#  signup_deadline   :datetime
+#  signup_deadline   :date
 #  timetables_public :boolean          default(FALSE)
+#  round             :integer
 #
 
 require 'spec_helper'
 
-describe Competition do
+describe Contest do
 
-  let (:competition) { FactoryGirl.build(:competition) }
+  let (:contest) { FactoryGirl.build(:contest) }
 
-  subject { competition }
+  subject { contest }
 
   # Attributes
   it { should respond_to(:season) }
@@ -45,7 +45,7 @@ describe Competition do
   it { should be_valid }
 
   describe "without an associated season" do
-    before { competition.season = nil }
+    before { contest.season = nil }
     it { should_not be_valid }
   end
 
@@ -53,53 +53,53 @@ describe Competition do
     it "should be invalid" do
       invalid_values = [-1, 1.5]
       invalid_values.each do |value|
-        competition.season = value
-        competition.should_not be_valid
+        contest.season = value
+        contest.should_not be_valid
       end
     end
   end
 
   describe "without an associated round" do
-    before { competition.round_id = nil }
+    before { contest.round_id = nil }
     it { should_not be_valid }
   end
 
   describe "without an associated host" do
-    before { competition.host_id = nil }
+    before { contest.host_id = nil }
     it { should_not be_valid }
   end
 
   describe "without an associated start date" do
-    before { competition.begins = nil }
+    before { contest.begins = nil }
     it { should_not be_valid }
   end
 
   describe "without an associated end date" do
-    before { competition.ends = nil }
+    before { contest.ends = nil }
     it { should_not be_valid }
   end
 
   describe "that ends before it begins" do
-    before { competition.ends = competition.begins - 1.day }
+    before { contest.ends = contest.begins - 1.day }
     it { should_not be_valid }
   end
 
   describe "that ends on the same day it begins" do
-    before { competition.ends = competition.begins }
+    before { contest.ends = contest.begins }
     it { should be_valid }
   end
 
   describe "without an associated signup deadline" do
-    before { competition.signup_deadline = nil }
+    before { contest.signup_deadline = nil }
     it { should_not be_valid }
   end
 
-  describe "with a signup deadline that is not before the competition begins" do
+  describe "with a signup deadline that is not before the contest begins" do
     it "should be invalid" do
-      invalid_deadlines = [competition.begins, competition.begins + 1.hour]
+      invalid_deadlines = [contest.begins, contest.begins + 1.hour]
       invalid_deadlines.each do |deadline|
-        competition.signup_deadline = deadline
-        competition.should_not be_valid
+        contest.signup_deadline = deadline
+        contest.should_not be_valid
       end
     end
   end
@@ -122,32 +122,32 @@ describe Competition do
     Competition.should respond_to(:preceding)
   end
 
-  it "should be able to return all competitions of the current season with given level" do
-    FactoryGirl.create(:past_competition)
-    FactoryGirl.create(:future_competition)
-    seasonal_rws = FactoryGirl.create_list(:current_competition, 2, round: FactoryGirl.create(:round))
-    seasonal_lws = FactoryGirl.create_list(:current_competition, 2, round: FactoryGirl.create(:second_round))
+  it "should be able to return all contests of the current season with given level" do
+    FactoryGirl.create(:past_contest)
+    FactoryGirl.create(:future_contest)
+    seasonal_rws = FactoryGirl.create_list(:current_contest, 2, round: FactoryGirl.create(:round))
+    seasonal_lws = FactoryGirl.create_list(:current_contest, 2, round: FactoryGirl.create(:second_round))
 
     Competition.seasonal_with_level(1).should =~ seasonal_rws
     Competition.seasonal_with_level(2).should =~ seasonal_lws
   end
 
-  it "should be able to return all competitions that are currently ongoing" do
-    FactoryGirl.create(:past_competition)
-    FactoryGirl.create(:future_competition)
-    current_competitions = FactoryGirl.create_list(:current_competition, 2)
+  it "should be able to return all contests that are currently ongoing" do
+    FactoryGirl.create(:past_contest)
+    FactoryGirl.create(:future_contest)
+    current_contests = FactoryGirl.create_list(:current_contest, 2)
 
-    Competition.current.should =~ current_competitions
+    Competition.current.should =~ current_contests
   end
 
-  it "should be able to return all current competitions whose signup is open" do
-    FactoryGirl.create(:past_competition)
-    FactoryGirl.create(:future_competition)
-    FactoryGirl.create(:deadlined_competition)
-    # TODO: Include competition whose deadline is exactly "now"
-    current_and_open_competitions = FactoryGirl.create_list(:current_competition, 2)
+  it "should be able to return all current contests whose signup is open" do
+    FactoryGirl.create(:past_contest)
+    FactoryGirl.create(:future_contest)
+    FactoryGirl.create(:deadlined_contest)
+    # TODO: Include contest whose deadline is exactly "now"
+    current_and_open_contests = FactoryGirl.create_list(:current_contest, 2)
 
-    Competition.current_and_open.should =~ current_and_open_competitions
+    Competition.current_and_open.should =~ current_and_open_contests
   end
 
   # Convenience methods
@@ -162,9 +162,9 @@ describe Competition do
 
   describe "should return a name to describe itself" do
     before do
-      competition.host = FactoryGirl.build(:host, name: "The Host")
-      competition.round = FactoryGirl.build(:round, slug: "Round Slug")
-      competition.season = 100
+      contest.host = FactoryGirl.build(:host, name: "The Host")
+      contest.round = FactoryGirl.build(:round, slug: "Round Slug")
+      contest.season = 100
     end
 
     its(:name) { should eq "The Host, Round Slug 2063" }
@@ -173,7 +173,7 @@ describe Competition do
   describe "should return the correct host name" do
     before do
       @host = FactoryGirl.build(:host, name: "The Host")
-      competition.host = @host
+      contest.host = @host
     end
 
     its(:host_name) { should eq @host.name }
@@ -181,58 +181,58 @@ describe Competition do
 
   it "should return the last full day when signup is possible" do
     [Date.today, Date.today + 1.hour].each do |deadline|
-      competition.signup_deadline = deadline
-      competition.last_signup_date.should eq Date.yesterday
+      contest.signup_deadline = deadline
+      contest.last_signup_date.should eq Date.yesterday
     end
   end
 
   describe "should return the days it takes place as a range" do
     before do
-      competition.begins = @today = Date.today
-      competition.ends = @in_3_days = Date.today + 3.days
+      contest.begins = @today = Date.today
+      contest.ends = @in_3_days = Date.today + 3.days
     end
 
     its(:days) { should eq (@today..@in_3_days) }
   end
 
   describe "should return the correct season year" do
-    before { competition.season = 100 }
+    before { contest.season = 100 }
     its(:year) { should eq 2063 }
   end
 
   describe "should return the correct round name with year" do
-    before { competition.season = 100 }
+    before { contest.season = 100 }
     its(:round_name_and_year) { should eq "Regionalwettbewerb 2063" }
   end
 
   it "should return whether performances can advance here" do
-    competition.round.level = 1
-    competition.can_be_advanced_to?.should be_false
+    contest.round.level = 1
+    contest.can_be_advanced_to?.should be_false
     [2, 3].each do |level|
-      competition.round.level = level
-      competition.can_be_advanced_to?.should be_true
+      contest.round.level = level
+      contest.can_be_advanced_to?.should be_true
     end
   end
 
   it "should return whether performances can advance onwards from here" do
-    competition.round.level = 1
-    competition.can_be_advanced_from?.should be_true
+    contest.round.level = 1
+    contest.can_be_advanced_from?.should be_true
     [2, 3].each do |level|
-      competition.round.level = level
-      competition.can_be_advanced_from?.should be_false
+      contest.round.level = level
+      contest.can_be_advanced_from?.should be_false
     end
   end
 
-  it "should be able to return all competitions of same season and next-higher round" do
-    next_round = FactoryGirl.create(:round, level: competition.round.level + 1)
-    possible_successors = FactoryGirl.create_list(:competition, 2, round: next_round)
+  it "should be able to return all contests of same season and next-higher round" do
+    next_round = FactoryGirl.create(:round, level: contest.round.level + 1)
+    possible_successors = FactoryGirl.create_list(:contest, 2, round: next_round)
 
     # We're not interested in these
-    FactoryGirl.create(:competition) # same round and season
-    FactoryGirl.create(:competition, round: next_round, season: competition.season + 1)
-    round_after_next = FactoryGirl.create(:round, level: competition.round.level + 2)
-    FactoryGirl.create(:competition, round: round_after_next)
+    FactoryGirl.create(:contest) # same round and season
+    FactoryGirl.create(:contest, round: next_round, season: contest.season + 1)
+    round_after_next = FactoryGirl.create(:round, level: contest.round.level + 2)
+    FactoryGirl.create(:contest, round: round_after_next)
 
-    competition.possible_successors.should =~ possible_successors
+    contest.possible_successors.should =~ possible_successors
   end
 end

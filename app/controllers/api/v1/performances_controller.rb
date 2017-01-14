54d343098bf05d3f8da7fcc2808193161a9a9cab
Martin Richter <me@martinrichter.net>
2017-01-14 14:13:48 +0100

    Enhance filtering of API /performances
    Performances returned from the API can now be filtered by venue+date,
    contest category, result publicity, or any combination thereof.

--- app/controllers/api/v1/performances_controller.rb
+++ app/controllers/api/v1/performances_controller.rb
@@ -6,27 +6,47 @@ module Api::V1
         return render nothing: true, status: :not_found
       end
 
+      ## Getting parameters
+
       venue = Venue.find(params[:venue_id]) rescue nil
 
-      # Parse given date assuming contest's local time zone
-      local_tz = contest.host.time_zone
+      local_tz = contest.host.time_zone # Assume contest's local time zone
       date = local_tz.parse(params[:date]) rescue nil
 
       contest_category = ContestCategory.find(params[:contest_category_id]) rescue nil
 
-      # TODO: Figure out param combinations that should lead to:
-      #
-      # return render nothing: true, status: :not_found
+      if results_public_filter = params[:results_public]
+        # Active filter has 'true' or 'false' value, 'nil' means inactive
+        results_public_value = results_public_filter == "1" ? true : false
+      end
 
+      ## Validation
+
+      # Require both venue and date if either is present
+      if !venue && date || venue && !date
+        return render nothing: true, status: :bad_request
+      end
+
+      ## Filtering
+
+      # Filter by venue and date
       if venue && date
         @performances = contest.staged_performances(venue, date)
       else
         @performances = contest.performances
       end
 
+      # Filter by contest category
       if contest_category
         @performances = @performances.in_contest_category(contest_category)
       end
+
+      # Filter by result publicity
+      if !results_public_value.nil?
+        @performances = @performances.where(results_public: results_public_value)
+      end
+
+      ## Fetching associations
 
       @performances = @performances.includes(
         { appearances: [:instrument, :participant] },
